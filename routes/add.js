af2007d57c0afc1721d6ce3c37cdfe71ab87b90e
Christopher Cai <chriscai1004@gmail.com>
2017-09-27 22:17:43 -0700

    minor change

--- routes/add.js
+++ routes/add.js
@@ -6,7 +6,7 @@ exports.add = function(req,res) {
 	console.log(req.body);
 	var body = req.body;
 	saveData(body.address, body.tname, body.mrent, body.pdate);
-	res.redirect('index');
+	res.redirect('/');
 }
 
 function saveData(address, name, rent, pdate) {
